56bebf760139552669f8dde98329e8e3fa73e0fe
Karl Kao <karl.kao@gmail.com>
2020-03-04 09:12:13 -0800

    Data point for tuning with aishell alpha / beta

--- examples/aishell/run_tune.sh
+++ examples/aishell/run_tune.sh
@@ -6,18 +6,18 @@ cd ../.. > /dev/null
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
 python -u tools/tune.py \
 --num_batches=-1 \
---batch_size=128 \
+--batch_size=256 \
 --beam_size=500 \
 --num_proc_bsearch=12 \
 --num_conv_layers=2 \
 --num_rnn_layers=3 \
 --rnn_layer_size=1024 \
---num_alphas=10 \
---num_betas=10 \
---alpha_from=0.1 \
---alpha_to=3.0 \
---beta_from=0.1 \
---beta_to=5.0 \
+--num_alphas=20 \
+--num_betas=20 \
+--alpha_from=0.0 \
+--alpha_to=10.0 \
+--beta_from=0.0 \
+--beta_to=10.0 \
 --cutoff_prob=1.0 \
 --cutoff_top_n=40 \
 --use_gru=True \
