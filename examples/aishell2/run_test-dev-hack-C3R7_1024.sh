b4e847eca1097f42f838fa2a21d826251e3c5af0
Karl Kao <karl.kao@gmail.com>
2020-03-15 22:45:22 -0700

    Data point for aishell2 profile

--- examples/aishell2/run_test-dev-hack-C3R7_1024.sh
+++ examples/aishell2/run_test-dev-hack-C3R7_1024.sh
@@ -5,17 +5,17 @@ cd ../.. > /dev/null
 # train model
 # if you wish to resume from an exists model, uncomment --init_from_pretrained_model
 export FLAGS_sync_nccl_allreduce=0
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
+CUDA_VISIBLE_DEVICES=0,1,2,4,5,6,7,8,9,10,11,12,13,14,15 \
 python -u train.py \
 --batch_size=16 \
---num_epoch=10 \
---num_conv_layers=2 \
---num_rnn_layers=3 \
+--num_epoch=1 \
+--num_conv_layers=3 \
+--num_rnn_layers=7 \
 --rnn_layer_size=1024 \
 --num_iter_print=100 \
 --save_epoch=1 \
---num_samples=120000 \
---learning_rate=5e-4 \
+--num_samples=225 \
+--learning_rate=1.8 \
 --max_duration=27.0 \
 --min_duration=0.0 \
 --test_off=False \
@@ -24,15 +24,15 @@ python -u train.py \
 --use_gpu=True \
 --is_local=True \
 --share_rnn_weights=False \
---train_manifest='data/aishell/manifest.train' \
---dev_manifest='data/aishell/manifest.dev' \
---mean_std_path='data/aishell/mean_std.npz' \
---vocab_path='data/aishell/vocab.txt' \
---output_model_dir='./checkpoints/aishell' \
+--train_manifest='data/Wang/16k/manifest.dev' \
+--dev_manifest='data/Wang/16k/manifest.dev' \
+--mean_std_path='data/aishell2/mean_std.npz' \
+--vocab_path='data/aishell2/vocab.txt' \
+--output_model_dir='./checkpoints/aishell2' \
 --augment_conf_path='conf/augmentation.config' \
 --specgram_type='linear' \
 --shuffle_method='batch_shuffle_clipped' \
---init_from_pretrained_model="./checkpoints/aishell/step_final"
+--init_from_pretrained_model="./checkpoints/aishell2-C3R71024/step_final"
 
 if [ $? -ne 0 ]; then
     echo "Failed in training!"
